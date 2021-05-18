49cd3890dc220788fbfcc3ab6cb6a320a5621ff1
Wangdi Tenzing <wangdi457@gmail.com>
2021-05-18 12:45:44 -0400

    react redux fake shop

---
+++ src/Components/ProductListing.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../Redux/Actions/productActions";
+import Product from "./Product";
+
+const ProductListing = () => {
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log(err);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Prdocuts", products);
+  return (
+    <div className=" ui grid container">
+      <Product />
+    </div>
+  );
+};
+
+export default ProductListing;
